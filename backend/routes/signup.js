c007ac620b9b8dfd2c64856d0a283529c01ca792
Marco Bonissi <bonis98@gmail.com>
2022-11-18 10:53:57 +0100

    Signup: various fixes
    Bcrypt async, added comments and json responses

--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -4,6 +4,7 @@ const controller = require('../middleware/controller');
 const express = require('express');
 const path = require("path");
 const bcrypt = require("bcrypt");
+const crypto = require('crypto');
 const {User} = require("../database/models/user");
 const router = express.Router();
 
@@ -17,55 +18,63 @@ router.get('/', function (req, res){
 
 
 router.post('/', function (req, res, next){
-        const generate_hash = function (password) {
-            return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
-        };
-        const ageCalculation = function (dateOfBirth){
-            const dob = moment(dateOfBirth, "DD-MM-YYYY").toDate();
-            const diff_ms = Date.now() - dob.getTime();
-            const age_dt = new Date(diff_ms);
-            return Math.abs(age_dt.getUTCFullYear() - 1970);
+    //Calculate age of the user
+    const ageCalculation = function (dateOfBirth){
+        //Convert date to js Date
+        const dob = moment(dateOfBirth, "DD-MM-YYYY").toDate();
+        //Calculate difference from current date
+        const diff_ms = Date.now() - dob.getTime();
+        //Create date from millis
+        const age_dt = new Date(diff_ms);
+        //Return difference
+        return Math.abs(age_dt.getUTCFullYear() - 1970);
+    }
+
+    const genderControl = function (gender){
+        return gender === 'M' || gender === 'F' || gender === '*';
+    }
+
+    User.findOne({
+        where: {
+            email: req.body.email
         }
-
-        const genderControl = function (gender){
-            return gender === 'M' || gender === 'F' || gender === '*';
-        }
-
-        User.findOne({
-            where: {
-                email: req.body.email
-            }
-        }).then(function(user) {
-            if (user) {
-                console.error('user already exists');
-                res.status(400).send("L'utente esiste già");
-            }else if (ageCalculation(req.body.dateOfBirth)<18) {
-                console.error('user is a baby');
-                res.status(400).send("L'utente è minorenne");
-            }else if (!genderControl(req.body.gender)){
-                console.error('gender not valid');
-                res.status(400).send("Il gender non è valido")
-            }else{
-                console.log('user does not exists and age is over 18');
-                const user_password = generate_hash(req.body.password);
-
-                const data = {
-                    email: req.body.email,
-                    password: user_password,
-                    name: req.body.name,
-                    surname: req.body.surname,
-                    dateOfBirth: moment(req.body.dateOfBirth, "DD-MM-YYYY").toDate().toISOString(),
-                    gender: req.body.gender,
-                    province: req.body.province
-                };
-                User.create(data).then(() => {
-                    res.sendStatus(200);
+    }).then(function(user) {
+        if (user) {
+            console.log('user already exists');
+            res.status(400).json({errortext: "L'utente esiste già"});
+        }else if (ageCalculation(req.body.dateOfBirth)<18) {
+            console.log('user is a baby');
+            res.status(400).json({errortext: "L'utente è minorenne"});
+        }else if (!genderControl(req.body.gender)){
+            console.log('gender not valid');
+            res.status(400).json({errortext: "Il gender non è valido"})
+        }else{
+            //All checks are passed, insert user in DB
+            console.log('user does not exists and age is over 18');
+            let userData = {
+                email: req.body.email,
+                name: req.body.name,
+                surname: req.body.surname,
+                dateOfBirth: moment(req.body.dateOfBirth, "DD-MM-YYYY").toDate().toISOString(),
+                gender: req.body.gender,
+                province: req.body.province,
+                token: crypto.randomBytes(20).toString('hex'),
+            };
+            //Calculate hash of the password
+            bcrypt.hash(req.body.password, bcrypt.genSaltSync(10)).then((hash) => {
+                userData.password = hash
+                User.create(userData).then(() => {
+                    res.status(200).json({token: userData.token});
                 },(error) => {
                     console.error(error)
                     res.sendStatus(500);
-                })
-            }
-        })
-})
+                });
+            }, (error) => {
+                console.error(error);
+                res.sendStatus(500);
+            })
+        }
+    });
+});
 
 module.exports = router; //eof
