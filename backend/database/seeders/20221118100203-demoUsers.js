004334a738188da3a6e58cc9af4a0da1f41ffda4
Marco Bonissi <bonis98@gmail.com>
2022-11-18 11:34:18 +0100

    Empty DB and population script

---
+++ backend/database/seeders/20221118100203-demoUsers.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Users', [{
+      email: "prova1@prova.com",
+      password: "$2b$10$81hUnKXLJx1uHRCn3.UP5.npCz7QLV8Oab1wwjCFj3zyDXD5wxQba", //"sicura"
+      name: "David",
+      surname: "Brown",
+      dateOfBirth: "1998-08-25",
+      province: "Bergamo",
+      gender: "M",
+      token: "ceb8437df3dca8646ecdece10373e695eaf24f54",
+      lockerList: "2;3;",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      email: "prova2@prova.com",
+      password: "$2b$10$81hUnKXLJx1uHRCn3.UP5.npCz7QLV8Oab1wwjCFj3zyDXD5wxQba", //"sicura"
+      name: "Mark",
+      surname: "Brown",
+      dateOfBirth: "1999-02-10",
+      province: "Bergamo",
+      gender: "M",
+      token: "ceb8437df3dca8646ecdece11673e695eaf24f54",
+      lockerList: "2;4;",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
