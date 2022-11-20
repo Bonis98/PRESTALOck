0309cafa92fed1a28665ed943a08ac6ab4f7e28b
Marco Bonissi <bonis98@gmail.com>
2022-11-20 18:32:32 +0100

    Added parameters checking

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -4,6 +4,10 @@ const {Product} = require("../database/models/product");
 const router = express.Router();
 
 router.post('/', function (req, res){
+    if (!checkParams(req)){
+        res.sendStatus(400);
+        return
+    }
     User.findOne({
         where: {
             token: req.get('token'),
@@ -25,5 +29,19 @@ router.post('/', function (req, res){
         console.error(error);
         res.sendStatus(500);
     })
-})
+});
+//Check request params
+function checkParams(req){
+    const body = req.body;
+    if (!(body.hasOwnProperty('title') && body.hasOwnProperty('description') && body.hasOwnProperty('maxLoanDays')))
+        return false
+    if (body.title.length == 0)
+        return false
+    if (body.description.length == 0)
+        return false
+    if (body.maxLoanDays <= 0)
+        return false
+    return true
+}
+
 module.exports = router; //eof
