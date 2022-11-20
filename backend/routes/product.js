1f73004e8e8e132ffb93674c9b89977c508c097a
Marco Bonissi <bonis98@gmail.com>
2022-11-20 18:33:10 +0100

    Added get product route

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {User} = require("../database/models/user");
 const {Product} = require("../database/models/product");
+const fetch = require("node-fetch");
 const router = express.Router();
 
 router.post('/', function (req, res){
@@ -49,6 +50,75 @@ router.put('/:id', function (req, res, next){
         res.sendStatus(500);
     })
 });
+
+router.get('/:id', function (req, res, next){
+    Product.findOne({
+        where: {
+            id: req.params.id,
+        },
+        include:[{
+            model: User,
+            required: true,
+            attributes: [['province', 'ownerProvince'], 'lockerList']
+        }],
+        attributes: {
+            exclude: ['createdAt', 'updatedAt'],
+        }
+    }).then((Product) => {
+        const user = Product.User.dataValues;
+        let result = {};
+        //Copy all keys except 'user' in result
+        for (let key in Product.dataValues)
+            if (key != 'User')
+                result[key] = Product[key]
+        //copy all keys in result
+        for (let key in user)
+            result[key] = user[key]
+        //Get locker list and filter it out by user's lockers
+        getLockerList(result.lockerList).then((lockerList) => {
+            result['lockerList'] = lockerList;
+            res.json(result);
+        }, (error) => {
+            console.error(error);
+            res.sendStatus(500);
+        });
+    }, (error) => {
+        console.error(error)
+        res.sendStatus(500);
+    })
+});
+
+//Filter out lockers list by user's lockers
+function getLockerList(list){
+    //Convert string of ids (1;2;) to int array
+    const lockerList = String(list).substring(0,list.length-1).split(';').map(Number);
+    let UserLockersList = {}
+    return new Promise((resolve, reject) => {
+        //retrieving complete lockers list, parsing it and sending it for filtering
+        fetch('http://hack-smartlocker.sintrasviluppo.it/api/lockers', {
+            method: 'get',
+            headers: {
+                "x-apikey": process.env.API_KEY_LOCKERS,
+                "x-tenant": process.env.TENANT
+            }
+        }).then((resp) => {
+            resp.json().then((jsonData) => {
+                for (let key in jsonData) {
+                    //Copy only user's lockers
+                    if (lockerList.includes(jsonData[key].id)) {
+                        UserLockersList[key] = jsonData[key]
+                    }
+                }
+                resolve(UserLockersList);
+            }, (error) => {
+                reject(error);
+            })
+        }, (error) => {
+            reject(error);
+        });
+    });
+}
+
 //Check request params
 function checkParams(req){
     const body = req.body;
