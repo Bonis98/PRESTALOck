aae406680576012f1ddabe9b829a4b80881781c3
Alessandro Sfriso <36988326+sfriso@users.noreply.github.com>
2022-11-21 20:25:58 +0100

    frontend: add new product page

---
+++ frontend/components/TextArea.vue
@@ -0,0 +1,16 @@
+<template>
+    <textarea class="w-72 border border-blue-400 rounded-full px-5 py-2"
+    :value="value"
+    @input="$emit('input', $event.target.value)"
+    style="resize: none">
+        
+    </textarea>
+
+</template>
+<script>
+export default {
+  props: {
+    value: String
+  }
+}
+</script>
