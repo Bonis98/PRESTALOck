710cf5ff000e770caa5974632864679ce40374c3
Alessandro Sfriso <36988326+sfriso@users.noreply.github.com>
2022-11-22 17:12:54 +0100

    frontend: fixed signup lint
    Co-Authored-By: emagon <emagon@emagon.xyz>

--- frontend/pages/signup.vue
+++ frontend/pages/signup.vue
@@ -3,39 +3,59 @@
     <Loader v-show="loading" />
     <TopBar />
     <div class="text-center w-11/12 m-auto mb-6 justify-around gap-24 pt-12">
-      <div class="mb-3 text-2xl"> Registrazione </div>
-      <div class="mt-3"> Indirizzo email </div>
+      <div class="mb-3 text-2xl">
+        Registrazione
+      </div>
+      <div class="mt-3">
+        Indirizzo email
+      </div>
       <div>
         <Input v-model="email" type="email" />
       </div>
-      <div class="mt-3"> Password </div>
+      <div class="mt-3">
+        Password
+      </div>
       <div>
         <Input v-model="password" type="password" />
       </div>
-      <div class="mt-3"> Conferma password </div>
+      <div class="mt-3">
+        Conferma password
+      </div>
       <div>
         <Input v-model="confirmPassword" type="password" />
       </div>
-      <div class="mt-12"> Nome </div>
+      <div class="mt-12">
+        Nome
+      </div>
       <div>
         <Input v-model="name" />
       </div>
-      <div class="mt-3"> Cognome </div>
+      <div class="mt-3">
+        Cognome
+      </div>
       <div>
         <Input v-model="surname" />
       </div>
-      <div class="mt-3"> Data di nascita </div>
+      <div class="mt-3">
+        Data di nascita
+      </div>
       <div>
         <Input v-model="dateOfBirth" />
       </div>
-      <div class="mt-3"> Provincia </div>
+      <div class="mt-3">
+        Provincia
+      </div>
       <div>
         <Select v-model="province" :options="allProvinces" />
       </div>
-      <div class="mt-3"> Genere </div>
+      <div class="mt-3">
+        Genere
+      </div>
       <div>
-        <Select v-model="gender"
-          :options="[{ val: '*', name: 'Altro' }, { val: 'M', name: 'Uomo' }, { val: 'F', name: 'Donna' }]" />
+        <Select
+          v-model="gender"
+          :options="[{ val: '*', name: 'Altro' }, { val: 'M', name: 'Uomo' }, { val: 'F', name: 'Donna' }]"
+        />
       </div>
       <div class="mt-12">
         <Button text="Registrati" />
@@ -50,7 +70,7 @@
 <script>
 
 export default {
-  data() {
+  data () {
     return {
       loading: true,
       email: '',
@@ -66,17 +86,26 @@ export default {
   },
 
   watch: {
-    loading(newValue) {
-      for (let element of document.querySelectorAll('Input, Button, Select')) {
+    loading (newValue) {
+      for (const element of document.querySelectorAll('Input, Button, Select')) {
         element.disabled = newValue
       }
     }
   },
 
+  created () {
+    localStorage.removeItem('token')
+    localStorage.removeItem('name')
+  },
+
+  mounted () {
+    this.getProvinces()
+  },
+
   methods: {
-    async getProvinces() {
+    async getProvinces () {
       // richiesta simulata
-      await new Promise((resolve => setTimeout(resolve, 1000)))
+      await new Promise(resolve => setTimeout(resolve, 1000))
       this.allProvinces = [
         { val: 'Milano', name: 'Milano' },
         { val: 'Cremona', name: 'Cremona' },
@@ -88,7 +117,7 @@ export default {
         { val: 'Arezzo', name: 'Arezzo' }
       ]
       this.loading = false
-    },/*
+    }, /*
     async getProvinces () {
       try {
         const response = await fetch('/api/provinces', {
@@ -109,7 +138,6 @@ export default {
 
         const data = await response.json()
         //data got
-        
 
       } catch (ex) {
         console.error(ex)
@@ -119,7 +147,7 @@ export default {
       }
     } */
 
-    async login() {
+    async login () {
       if (this.password != this.confirmPassword) {
         alert('Le password devono coincidere')
         return
@@ -179,27 +207,16 @@ export default {
         }
 
         const data = await response.json()
-        //data got
+        // data got
         localStorage.setItem('token', data.token)
         localStorage.setItem('name', data.name + ' ' + data.surname)
         this.$router.push({ path: '/login' })
-      }
-      catch (ex) {
+      } catch (ex) {
         console.error(ex)
         alert(ex)
         this.loading = false
-        return
       }
     }
-  },
-
-  created() {
-    localStorage.removeItem('token')
-    localStorage.removeItem('name')
-  },
-
-  mounted() {
-    this.getProvinces()
   }
 }
 </script>
