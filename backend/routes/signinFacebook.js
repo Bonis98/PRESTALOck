68e82d3be0a4074a4fdde29c355904e6d535ec9b
Marco Bonissi <bonis98@gmail.com>
2022-11-26 22:04:29 +0100

    Backend: added Facebook oauth

---
+++ backend/routes/signinFacebook.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const router = express.Router();
+const {User} = require("../database/models/user");
+const moment = require("moment");
+const crypto = require("crypto");
+
+router.get('/', function (req, res){
+    const state = crypto.randomBytes(30).toString('hex')
+    res.cookie('state', state, {signed: true})
+    res.redirect('https://www.facebook.com/v15.0/dialog/oauth?' + new URLSearchParams({
+        client_id: process.env.FACEBOOK_CLIENT_ID,
+        redirect_uri: 'https://localhost/api/signinFacebook/callback',
+        state: state,
+        scope: 'email,user_birthday,user_gender'
+    }))
+})
+
+router.get('/callback', async function (req, res) {
+    res.clearCookie('state')
+    //Check state parameter
+    if (req.signedCookies.state != req.query.state){
+        res.sendStatus(401)
+        return
+    }
+    try {
+        //Get access token from auth code
+        let response = await fetch('https://graph.facebook.com/v15.0/oauth/access_token?' + new URLSearchParams({
+            client_id: process.env.FACEBOOK_CLIENT_ID,
+            redirect_uri: 'https://localhost/api/signinFacebook/callback',
+            client_secret: process.env.FACEBOOK_CLIENT_SECRET,
+            code: req.query.code,
+        }))
+        let body = await response.json()
+        let access_token = body.access_token
+        //Get user id
+        response = await fetch('https://graph.facebook.com/me?' + new URLSearchParams({
+            access_token: access_token
+        }))
+        body = await response.json()
+        //Get user data
+        response = await fetch('https://graph.facebook.com/' + body.id + '?' + new URLSearchParams({
+            fields: 'first_name,last_name,email,birthday,gender',
+            access_token: access_token
+        }))
+        body = await response.json()
+        let userInfo = {}
+        userInfo['email'] = body.email
+        userInfo['name'] = body.first_name
+        userInfo['surname'] = body.last_name
+        userInfo['dateOfBirth'] = moment(body.birthday, 'MM/DD/YYYY').format('DD-MM-YYYY')
+        switch (body.gender) {
+            case 'male':
+                userInfo['gender'] = 'M'
+                break
+            case 'female':
+                userInfo['gender'] = 'F'
+                break
+            default:
+                userInfo['gender'] = '*'
+                break
+        }
+        //Check if user already exists
+        const user = await User.findOne({
+            where: {
+                email: userInfo.email
+            }
+        })
+        //If already exists, send back token
+        if (user){
+            console.log('user already exists');
+            let base64Data = Buffer.from(JSON.stringify({token: user.token})).toString("base64")
+            res.redirect('/afterfacebooklogin?data=' + base64Data)
+            return;
+        }
+        let base64Data = Buffer.from(JSON.stringify(userInfo)).toString("base64")
+        res.redirect('/afterfacebooklogin?data=' + base64Data)
+    } catch (error) {
+        console.error(error)
+        res.sendStatus(500)
+    }
+})
+
+module.exports = router;
