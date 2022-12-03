0779590a6d1ce4bf5621a3ef1efdfbef1f038856
Davide <43414189+Demiurgo443@users.noreply.github.com>
2022-12-03 01:40:42 +0100

    Backend: checkpoint book/return
    - need to be tested

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -5,15 +5,33 @@ const {UserBorrowProduct} = require("../database/models/userBorrowProduct");
 const {User} = require("../database/models/user");
 const sendMail = require("../utils/mailUtils");
 const {sequelize} = require("../database/connection");
+const {Op} = require("sequelize");
 const router = express.Router();
 
 class NoSlotError extends Error {
     constructor(args){
         super(args);
-        this.name = "NoSlotError"
+        this.name = "NoSlotError";
     }
 }
 
+//search and return the first free slot
+function searchSlot(json) {
+    let slotData;
+    for (let i = 0; i < json.length; i++) {
+        if (json[i].status.booked === false) {
+            slotData = json[i];
+            return slotData;
+        }
+    }
+    return false;
+}
+
+function checkSlotAvailability(slot){
+    if (!slot) {
+        throw new NoSlotError("No available slots found.");
+    }
+}
 
 router.post('/', async function (req, res) {
     let UserBorrowProductData;
@@ -27,18 +45,6 @@ router.post('/', async function (req, res) {
             },
             attributes: ['id', 'name', 'surname', 'email']
         });
-    }
-
-    //search and return the first free slot
-    function searchSlot(json) {
-        let slotData;
-        for (let i = 0; i < json.length; i++) {
-            if (json[i].status.booked == false) {
-                slotData = json[i];
-                return slotData;
-            }
-        }
-        return false;
     }
 
     //check product has no other active book //hookable
@@ -101,16 +107,14 @@ router.post('/', async function (req, res) {
 
         const jsonData = await resp.json();
         let slot = await searchSlot(jsonData);
-        if (!slot) {
-            throw new NoSlotError("No available slots found.");
-        }
+        await checkSlotAvailability(slot);
 
         bodyPostBook = {
             customerId: userReceiver.id,
             customerName: userReceiver.name + " " + userReceiver.surname,
             slotId: slot.id,
             slotContent: product.title
-        }
+        };
 
         //book the slot on Sintra API
         const respPost = await fetch('http://hack-smartlocker.sintrasviluppo.it/api/book', {
@@ -143,7 +147,7 @@ router.post('/', async function (req, res) {
             idUser: userReceiver.id,
             idProduct: product.id,
             lockerSlot: slot.id
-        }
+        };
 
         //creating new entry in UserBorrowProduct (new book)
         await UserBorrowProduct.create(UserBorrowProductData, {transaction: t});
@@ -170,28 +174,28 @@ router.post('/', async function (req, res) {
         }
 
         //setting email for reciever
-        let emailSubject = "Prenotazione confermata"
-        let emailText = "Il proprietario dell'oggetto è stato notificato della richiesta di prestito. " +
-            "Ti verrà inviata una seconda mail quando l'oggetto sarà pronto per il ritiro."
+        let emailSubject = "Prenotazione confermata";
+        let emailText = `Il proprietario dell'oggetto è stato notificato della richiesta di prestito. 
+        Ti verrà inviata una seconda mail quando l'oggetto sarà pronto per il ritiro.`;
         const mailObjReceiver = {
             from: process.env.MAIL_USER,
             to: userReceiver.email,
             subject: emailSubject,
             text: emailText
-        }
+        };
         await sendMail(mailObjReceiver);
 
         //setting email for owner
-        emailSubject = "Richiesta di prestito"
-        emailText = "L'oggetto " + product.title + " è stato prenotato. Recati al locker " + locker.nome +
-            " sito in via " + locker.address + " e depositalo nello slot " +
-            slot.index + ". Il codice per aprire lo slot è il seguente: " + receiverUnlockCode + "."
+        emailSubject = "Richiesta di prestito";
+        emailText = `L'oggetto ${product.title} è stato prenotato. Recati al locker ${locker.nome} sito in 
+        via ${locker.address} e depositalo nello slot ${slot.index}. 
+        Il codice per aprire lo slot è il seguente: ${receiverUnlockCode}.`;
         const mailObjOwner = {
             from: process.env.MAIL_USER,
             to: product.user.email,
             subject: emailSubject,
             text: emailText
-        }
+        };
         await sendMail(mailObjOwner);
 
         await t.commit()
@@ -206,6 +210,148 @@ router.post('/', async function (req, res) {
             res.sendStatus(500);
         }
     }
-})
+});
+
+router.get('/return/:idProduct', async function(req, res){
+
+    try {
+        const currentUser = await User.findOne({
+            where: {
+                token: req.get('Auth-Token')
+            }
+        });
+
+        const activeBook = await UserBorrowProduct.findOne({
+            where: {
+                idUser: currentUser.id,
+                idProduct: req.params.idProduct,
+                loanStartDate: {
+                    [Op.not]: null
+                },
+                terminationDate: null
+            }, include: [{
+                model: Product,
+                required: true,
+                attributes: ['title'],
+                include: [{
+                    model: User,
+                    required: true,
+                    attributes: ['email']
+                }]
+            }], attributes: {
+                exclude: ['requestDate', 'createdAt', 'updatedAt']
+            }
+        });
+
+        if(!activeBook){
+            res.sendStatus(404); //not found
+            return;
+        }
+
+        //retrieving complete lockers list
+        let lockerList = await fetch('http://hack-smartlocker.sintrasviluppo.it/api/lockers', {
+            method: 'get',
+            headers: {
+                "x-apikey": process.env.API_KEY_LOCKERS,
+                "x-tenant": process.env.TENANT
+            }
+        });
+
+        const urlInfoSlot = 'http://hack-smartlocker.sintrasviluppo.it/api/slots/' + activeBook.lockerSlot;
+        //retrieve slot info by its id
+        let infoSlot = await fetch(urlInfoSlot, {
+            method: 'get',
+            headers: {
+                "x-apikey": process.env.API_KEY_LOCKERS,
+                "x-tenant": process.env.TENANT
+            }
+        })
+
+        const urlSlotsByLocker = 'http://hack-smartlocker.sintrasviluppo.it/api/slots?lockerId=' + infoSlot.lockerId;
+        //retrieve all slots for locker already used
+        let slots = await fetch(urlSlotsByLocker, {
+            method: 'get',
+            headers: {
+                "x-apikey": process.env.API_KEY_LOCKERS,
+                "x-tenant": process.env.TENANT
+            }
+        })
+
+
+        let slot = await searchSlot(slots);
+        await checkSlotAvailability(slot);
+
+        const bodyPostBook = {
+            customerId: currentUser.id,
+            customerName: currentUser.name + " " + currentUser.surname,
+            slotId: slot.id,
+            slotContent: activeBook.product.title
+        };
+
+        //book the slot on Sintra API
+        const respPost = await fetch('http://hack-smartlocker.sintrasviluppo.it/api/book', {
+            method: 'post',
+            body: JSON.stringify(bodyPostBook),
+            headers: {
+                "x-apikey": process.env.API_KEY_LOCKERS,
+                "x-tenant": process.env.TENANT,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (respPost.status !== 200) {
+            res.status(503).json({errorText: "Errore temporaneo, riprova"}); // Service unavailable
+            return;
+        }
+        const jsonSlotData = await respPost.json();
+
+        await activeBook.update({returnLockerSlot: slot.id});
+        await activeBook.save();
+
+        lockerList = await lockerList.json();
+        //retrieve locker data -> send its name and address through email
+        let locker;
+        for (let i=0; i<lockerList.length; i++){
+            if (lockerList[i].id === infoSlot.lockerId){
+                locker = lockerList[i]
+                break
+            }
+        }
+
+        let receiverUnlockCode = jsonSlotData.status.unlockCodes[0];
+
+        //setting email for borrower
+        let emailSubject = "Prenotazione slot locker confermata";
+        let emailText = `Hai prenotato con successo lo slot ${slot.index} del locker ${locker.nome} sito in via
+         ${locker.address}. Il proprietario di ${activeBook.product.title} è stato notificato 
+         dell'avvio della procedura di restituzione. 
+         Il codice di sblocco che dovrai usare è il seguente: ${receiverUnlockCode}`;
+        const mailObjBorrower = {
+            from: process.env.MAIL_USER,
+            to: currentUser.email,
+            subject: emailSubject,
+            text: emailText
+        };
+        await sendMail(mailObjBorrower);
+
+        //setting email for owner
+        emailSubject = "Notifica avvio restituzione";
+        emailText = `L'oggetto ${activeBook.product.title} ha iniziato l'iter di restituzione. Ti manderemo una email 
+        quando sarà stato depositato.`;
+        const mailObjOwner = {
+            from: process.env.MAIL_USER,
+            to: activeBook.product.user.email,
+            subject: emailSubject,
+            text: emailText
+        };
+        await sendMail(mailObjOwner);
+
+        res.sendStatus(200);
+
+    } catch (error) {
+
+    }
+
+});
 
 module.exports = router;
