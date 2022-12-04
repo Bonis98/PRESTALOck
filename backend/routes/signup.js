a8ca3d8d414ff0969bf338b6350ab669ebcaee7d
Davide <43414189+Demiurgo443@users.noreply.github.com>
2022-12-04 15:10:04 +0100

    Backend: added signup welcome email

--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const crypto = require('crypto');
 const {User} = require("../database/models/user");
+const sendMail = require("../utils/mailUtils");
 const signupUtils = require("../utils/signupUtils");
 const router = express.Router();
 
@@ -37,6 +38,32 @@ router.post('/', function (req, res){
                 userData.password = req.body.password
             }
             User.create(userData).then((userData) => {
+                let vocal;
+                switch (userData.gender) {
+                    case "M":
+                        vocal = "o";
+                        break;
+                    case "F":
+                        vocal = "a";
+                        break;
+                    default:
+                        vocal = "*";
+                }
+                const emailSubject = "Registrazione avvenuta con successo!"
+                const emailText = `Benvenut${vocal} ${userData.name} ${userData.username}! La ringraziamo per essersi `+
+                    `registrat${vocal} sulla nostra piattaforma. Speriamo che il contenuto e le funzionalità `+
+                    `possano essere di suo gradimento. In caso contrario non esiti a contattarci.
+                    Buona permanenza! `+
+                    `Distinti saluti, 
+                    Assistenza clienti PRESTALOck`;
+                const mailObj = {
+                    from: process.env.MAIL_USER,
+                    to: userData.email,
+                    subject: emailSubject,
+                    text: emailText
+                };
+                sendMail(mailObj).catch((error) => console.log(error))
+
                 res.status(200).json({
                     token: userData.token,
                     userId: userData.id,
